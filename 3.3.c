ba9210fba9e7b6c11146a0636a257d35b4cee524
Taimoor Masaud <55857068+TaimoorMasaud@users.noreply.github.com>
2019-10-26 23:51:19 +0500

    Add files via upload

---
+++ 3.3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n1=40,n2=20,a=0,b=0,c=0,d=0,e=0;
+    a=n1+n2;
+    b=n1-n2;
+    c=n1*n2;
+    d=n1/n2;
+    e=n1%n2;
+    printf("Addition of a,b is: %d\n",a);
+    printf("Subtraction of a,b is: %d\n",b);
+    printf("Multiplication of a,b is: %d\n",c);
+    printf("Division of a,b is: %d\n",d);
+    printf("Modulus of a,b is: %d\n",e);
+}
